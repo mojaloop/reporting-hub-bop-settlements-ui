2ce0c2bba1ac64c8991f7237a4d8f7b5c0a29334
Kalin Krustev <kalin.krustev@gmail.com>
2025-07-30 13:31:52 +0000

    fix: support read only root filesystem

--- docker/loadRuntimeConfig.sh
+++ docker/loadRuntimeConfig.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#! /bin/sh
 if grep -q "__CENTRAL_SETTLEMENTS_ENDPOINT__" /usr/share/nginx/html/runtime-env.js; then
-sed -i 's#__CENTRAL_SETTLEMENTS_ENDPOINT__#'"$CENTRAL_SETTLEMENTS_ENDPOINT"'#g' /usr/share/nginx/html/runtime-env.js
-sed -i 's#__CENTRAL_LEDGER_ENDPOINT__#'"$CENTRAL_LEDGER_ENDPOINT"'#g' /usr/share/nginx/html/runtime-env.js
-sed -i 's#__REPORTING_TEMPLATE_API_ENDPOINT__#'"$REPORTING_TEMPLATE_API_ENDPOINT"'#g' /usr/share/nginx/html/runtime-env.js
+  sed -i 's#__CENTRAL_SETTLEMENTS_ENDPOINT__#'"$CENTRAL_SETTLEMENTS_ENDPOINT"'#g' /usr/share/nginx/html/runtime-env.js;
+  sed -i 's#__CENTRAL_LEDGER_ENDPOINT__#'"$CENTRAL_LEDGER_ENDPOINT"'#g' /usr/share/nginx/html/runtime-env.js;
+  sed -i 's#__REPORTING_TEMPLATE_API_ENDPOINT__#'"$REPORTING_TEMPLATE_API_ENDPOINT"'#g' /usr/share/nginx/html/runtime-env.js;
 else
-  echo "skipping replacement."
+  echo "skipping replacement.";
 fi
 
 exec "$@"
